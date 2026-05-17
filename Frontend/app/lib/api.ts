c24ce2f6e18279e3f086e2379c6722f4710452c1
ExcWasHere <excellchriatian12@gmail.com>
2026-05-17 14:59:35 +0700

    fix(api): update API_URL to use environment variable

--- Frontend/app/lib/api.ts
+++ Frontend/app/lib/api.ts
@@ -1,30 +1,23 @@
-const API_URL = "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const apiFetch = async (
   endpoint: string,
   options: RequestInit = {}
 ) => {
-  const token = localStorage.getItem(
-    "access_token"
-  );
+  const token = localStorage.getItem("access_token");
 
   const headers = {
     "Content-Type": "application/json",
-
     ...(token && {
       Authorization: `Bearer ${token}`,
     }),
-
     ...(options.headers || {}),
   };
 
-  const response = await fetch(
-    `${API_URL}${endpoint}`,
-    {
-      ...options,
-      headers,
-    }
-  );
+  const response = await fetch(`${API_URL}${endpoint}`, {
+    ...options,
+    headers,
+  });
 
   return response;
 };
